569487edab4e4f655af4c892bec647ef13448651
abubienczyk <olabubienczyk@gmail.com>
2024-04-05 13:05:18 +0200

    commit

---
+++ REST API/REST API/Controlers/AnimalControler.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace REST_API.Controlers;
+
+public class AnimalControler
+{
+    //[ApiController]
+    //[HttpPatch("animals")]
+    
+}
---
+++ REST API/REST API/Models/Animal.cs
@@ -0,0 +1,10 @@
+namespace REST_API.Models;
+
+public class Animal
+{
+    public int id { get; set; }
+    public string name { get; set; }
+    public string category { get; set; }
+    public double weight{ get; set; }
+    public string fur { get; set; }
+}
